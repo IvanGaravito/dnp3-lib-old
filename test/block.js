66156c8708f7dea26082ebaf2c51c9b6f2b41ee6
IvanGaravito <ivangaravito@gmail.com>
2015-10-26 21:14:44 -0600

    Modified Block test suit to group tests by function description

--- test/block.js
+++ test/block.js
@@ -9,68 +9,74 @@ describe('Block', function () {
     Block = require('../lib/Block')
   })
 
-  it('should have dataSize equal to initial size', function () {
-    var block
+  describe('#initialization', function () {
+    it('should have dataSize equal to initial size', function () {
+      var block
 
-    block = new Block(8)
-    assert(block.dataSize === 8)
+      block = new Block(8)
+      assert(block.dataSize === 8)
+    })
+
+    it('should have buffer size equal to dataSize + 2', function () {
+      var block
+
+      block = new Block(16)
+      assert(block._data.length === 18)
+    })
   })
 
-  it('should have buffer size equal to dataSize + 2', function () {
-    var block
+  describe('#data()', function () {
+    it('should reject data with wrong size', function () {
+      var block
 
-    block = new Block(16)
-    assert(block._data.length === 18)
+      block = new Block(8)
+      assert.equal(block.data('056405C0FD02BC028B4F'), Block.Error.WrongDataSize)
+    })
+
+    it('should accept data with dataSize length', function () {
+      var block
+
+      block = new Block(8)
+      assert(block.data('056405C0FD02BC02'))
+    })
+
+    it('should accept data and generate correct CRC', function () {
+      var block
+
+      block = new Block(8)
+      block.data('056405C0FD02BC02')
+      assert.equal(block.crc().toString(16).toUpperCase(), '4F8B')
+    })
+
+    it('should return data when calling .data without arguments', function () {
+      var block
+
+      block = new Block(8)
+      block.data('056405C0FD02BC02')
+      assert.equal(block.data().toString('hex').toUpperCase(), '056405C0FD02BC02')
+    })
   })
 
-  it('should reject data with wrong size', function () {
-    var block
+  describe('#load()', function () {
+    it('should reject load raw data not equal to dataSize + 2', function () {
+      var block
 
-    block = new Block(8)
-    assert.equal(block.data('056405C0FD02BC028B4F'), Block.Error.WrongDataSize)
-  })
+      block = new Block(8)
+      assert.equal(block.load('056405C0FD02BC02'), Block.Error.WrongBlockSize)
+    })
 
-  it('should accept data with dataSize length', function () {
-    var block
+    it('should load raw data and check CRC is correct', function () {
+      var block
 
-    block = new Block(8)
-    assert(block.data('056405C0FD02BC02'))
-  })
+      block = new Block(8)
+      assert(block.load('056405C0FD02BC028B4F'))
+    })
 
-  it('should accept data and generate correct CRC', function () {
-    var block
+    it('should load raw data and check CRC is incorrect', function () {
+      var block
 
-    block = new Block(8)
-    block.data('056405C0FD02BC02')
-    assert.equal(block.crc().toString(16).toUpperCase(), '4F8B')
-  })
-
-  it('should return data when calling .data without arguments', function () {
-    var block
-
-    block = new Block(8)
-    block.data('056405C0FD02BC02')
-    assert.equal(block.data().toString('hex').toUpperCase(), '056405C0FD02BC02')
-  })
-
-  it('should reject load raw data not equal to dataSize + 2', function () {
-    var block
-
-    block = new Block(8)
-    assert.equal(block.load('056405C0FD02BC02'), Block.Error.WrongBlockSize)
-  })
-
-  it('should load raw data and check CRC is correct', function () {
-    var block
-
-    block = new Block(8)
-    assert(block.load('056405C0FD02BC028B4F'))
-  })
-
-  it('should load raw data and check CRC is incorrect', function () {
-    var block
-
-    block = new Block(8)
-    assert.equal(block.load('056405C0000001008B4F'), false)
+      block = new Block(8)
+      assert.equal(block.load('056405C0000001008B4F'), false)
+    })
   })
 })
